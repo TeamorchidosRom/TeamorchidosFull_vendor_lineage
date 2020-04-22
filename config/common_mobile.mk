7e565b99f5dec33ec6022cee2177c5f83f5b0e06
DJABhipHop <castellanos.kevin@rocketmail.com>
2020-04-21 20:51:47 -0400

    Split mobile and tv configs
    There are a lot of apps installed by default that are unusable with
    a controller. This splits the mobile related settings out of common
    into a common_mobile config and adds common_tv.
    
    Note: The EXCLUDE_AUDIOFX flag was removed from the tv configs
    because the new config path no longer builds it.
    
    Change-Id: I67db32750fca91725d703243f2bcee8bcd9b82cd

---
+++ config/common_mobile.mk
@@ -0,0 +1,55 @@
+# Inherit common mobile Lineage stuff
+$(call inherit-product, vendor/lineage/config/common.mk)
+
+# Default notification/alarm sounds
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Hassium.ogg
+
+ifneq ($(TARGET_BUILD_VARIANT),user)
+# Thank you, please drive thru!
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.dun.override=0
+endif
+
+# Optional packages
+PRODUCT_PACKAGES += \
+    LiveWallpapersPicker \
+    PhotoTable
+
+# AOSP packages
+PRODUCT_PACKAGES += \
+    Email \
+    ExactCalculator \
+    Exchange2
+
+# Lineage packages
+PRODUCT_PACKAGES += \
+    AudioFX \
+    Backgrounds \
+    Eleven \
+    OpenCamera \
+    Jelly \
+    LockClock \
+    Profiles \
+    TrebuchetQuickStep \
+    Longshot
+
+# Overlays
+PRODUCT_PACKAGES += \
+    lightning-overlays
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+# Custom off-mode charger
+ifeq ($(WITH_LINEAGE_CHARGER),true)
+PRODUCT_PACKAGES += \
+    lineage_charger_res_images \
+    font_log.png \
+    libhealthd.lineage
+endif
+
+# Media
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    media.recorder.show_manufacturer_and_model=true
