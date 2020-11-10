f186bbcbb3023a630c82bd32a1a336fc0d56ce00
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2020-11-10 17:53:07 -0500

    vendor: Enabling on SkiaGL by default
    Thanks to @VagnerMA fo the suggestion

--- config/common.mk
+++ config/common.mk
@@ -177,6 +177,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.storage_manager.enabled=true
 
+# Skiagl 
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    	debug.hwui.renderer=skiagl
+
 # These packages are excluded from user builds
 PRODUCT_PACKAGES_DEBUG += \
     procmem
