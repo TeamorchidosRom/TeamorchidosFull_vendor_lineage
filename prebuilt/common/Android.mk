830216f4f3c100598fc8b43ac54593201b7c1ecc
teamorchidos <71541333+teamorchidos@users.noreply.github.com>
2020-12-20 18:34:50 +0000

    dont

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -39,30 +39,6 @@ LOCAL_PRODUCT_MODULE := true
 
 include $(BUILD_PREBUILT)
 
-# Adding klausw/HackersKeyboard as Prebuilt for now will add as source in newer android versions if I can.
-include $(CLEAR_VARS)
-LOCAL_MODULE := HackersKeyboard
-LOCAL_MODULE_OWNER := lineage
-LOCAL_SRC_FILES := apps/$(LOCAL_MODULE).apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_OVERRIDES_PACKAGES := LatinImeGoogle
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_CERTIFICATE := PRESIGNED
-include $(BUILD_PREBUILT)
-
-# Adding mark-h/OpenCamera as Prebuilt for now will add as source in newer android versions if I can.
-include $(CLEAR_VARS)
-LOCAL_MODULE := OpenCamera
-LOCAL_MODULE_OWNER := lineage
-LOCAL_SRC_FILES := apps/$(LOCAL_MODULE).apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := PRESIGNED
-include $(BUILD_PREBUILT)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := fonts_customization.xml
 LOCAL_SRC_FILES := etc/fonts_customization.xml
